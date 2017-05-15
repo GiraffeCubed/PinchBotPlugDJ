7120816f696d5468d19e018cc366bda71e85d465
GiraffeCubed <jordan.mccormack261@gmail.com>
2017-05-15 22:38:21 +0100

    Update extension.js

--- extension.js
+++ extension.js
@@ -34,7 +34,8 @@
             if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
             if (!bot.commands.executable(this.rank, chat)) return void (0);
             else {
-              API.sendChat("http://i.imgur.com/wZSVKQF.jpg");
+              #API.sendChat("http://i.imgur.com/wZSVKQF.jpg");
+              API.chatLog("http://i.imgur.com/wZSVKQF.jpg");
             }
           }
         };
